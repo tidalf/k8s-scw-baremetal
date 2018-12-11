596bd5c2a4516a069570f1421d944c6ca983e1b5
cyril <cyril.leclerc@gmail.com>
2018-12-11 01:48:35 +0100

    aws

--- terraform.tf
+++ terraform.tf
@@ -1,3 +1,15 @@
+provider "aws" {
+  region = "eu-west-3"
+  shared_credentials_file = "~/.aws/credentials"
+  profile = "aws"
+}
+
 terraform {
-  required_version = "<= 0.11.9"
+  required_version = "<= 0.11.10"
+   backend "s3" {
+    bucket = "terraformstateboobox"
+    key    = "key"
+    region = "eu-west-3"
+    profile = "aws"
+  } 
 }
